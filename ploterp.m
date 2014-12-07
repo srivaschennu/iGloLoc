bb76e535a7d810e945ebd50f79406608c71cfa98
Srivas Chennu <srivas@gmail.com>
2014-12-07 00:00:06 +0000

    Checked in Sun Dec  7 00:00:05 GMT 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/iGloLoc@33 06446dee-3473-49d2-9db8-f6f8861ec768

--- ploterp.m
+++ ploterp.m
@@ -57,11 +57,11 @@ for s = 1:numsubj
     % %     rereference
     %     EEG = rereference(EEG,1);
     
-        %%%%% baseline correction relative to 5th tone
-%         bcwin = [-200 0];
-%         bcwin = bcwin+timeshift;
-%         EEG = pop_rmbase(EEG,bcwin);
-        %%%%%
+        %%%% baseline correction relative to 5th tone
+        bcwin = [-200 0];
+        bcwin = bcwin+timeshift;
+        EEG = pop_rmbase(EEG,bcwin);
+        %%%%
     
     % THIS ASSUMES THAT ALL DATASETS HAVE SAME NUMBER OF ELECTRODES
     if s == 1
